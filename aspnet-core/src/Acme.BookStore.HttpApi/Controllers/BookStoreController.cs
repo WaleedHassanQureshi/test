2d98c55eedae74daf31f56ee43e8434e3f37bdd3
WaleedHassanQureshi <134970976+WaleedHassanQureshi@users.noreply.github.com>
2023-05-30 02:42:35 +0500

    Abp Angular Api

---
+++ aspnet-core/src/Acme.BookStore.HttpApi/Controllers/BookStoreController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Xml.Linq;
+using Volo.Abp;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace Acme.BookStore.Controllers
+{
+    [RemoteService(Name = "Acme.BookStore")]
+    [Area("your-area-name")]
+    [Route("api/[controller]")]
+    public class DataController : AbpController
+    {
+        [HttpGet("chart-data")]
+        public IActionResult GetChartData()
+        {
+            // Read the JSON data from the file
+            string jsonFilePath = @"C:\Users\Waleed Hassan\Downloads\Chart Four - Data.txt";
+            string jsonData = System.IO.File.ReadAllText(jsonFilePath);
+
+            // Return the JSON data
+            return Ok(jsonData);
+        }
+    }
+}
